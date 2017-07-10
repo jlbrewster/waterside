b3a0431f704f980d502d99162ef7199c66df13c1
Jeff Brewster <jeff.brewster@kareo.com>
2017-07-10 13:54:56 -0700

    Theme changes

--- docroot/themes/custom/blue/js/main.js
+++ docroot/themes/custom/blue/js/main.js
@@ -1,1 +1,11 @@
 // MAIN.js file - for aggregation/GULP builds
+
+// This is the loader for web font licensing through fonts.com
+var MTIProjectId='287cd7ff-fd1c-4d52-a0fa-9a2a380c2c0c';
+        (function() {
+        var mtiTracking = document.createElement('script');
+        mtiTracking.type='text/javascript';
+        mtiTracking.async='true';
+        mtiTracking.src='/assets/fonts/mtiFontTrackingCode.js';
+        (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild( mtiTracking );
+        })();
